8c6c99d3b3ebb7ba1ddfabf0798d9eb03f128771
jelly <chm073@gmail.com>
2023-08-10 22:18:01 +0900

    Add Admin Login Page

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+</script>
+
+<div class="flex flex-col text-left items-center text-2xl p-8 gap-4">
+  <a href="/admin">관리자 로그인-설치 후 최초 로그인</a>
+  <a href="/admin/league/register">
+    로그인 > 관리자 로그인 > 첫 리그 정보 등록
+  </a>
+</div>
