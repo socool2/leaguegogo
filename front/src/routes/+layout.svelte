a0993694bafad1f4887ad321e5457534619d1aab
jelly <chm073@gmail.com>
2023-08-13 00:50:23 +0900

    Add Navigation

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -1,4 +1,13 @@
 <script>
   import "../app.css";
+  import Navigation from "$lib/components/Navigation.svelte";
+
+
+
 </script>
+
+
+
+<Navigation />
+
 <div class={"overflow-auto bg-gradient-to-br from-cyan-100 to-blue-50 h-screen w-screen"}><slot></slot></div>
