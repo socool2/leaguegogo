df4e7aaaf03608240e36a9d405bccfb8eed14db7
jelly <chm073@gmail.com>
2023-08-15 13:11:06 +0900

    Update Structures

---
+++ front/src/routes/(register)/+layout.svelte
@@ -0,0 +1,1 @@
+<div class={"overflow-auto bg-gradient-to-br from-cyan-100 to-blue-50 h-screen w-screen"}><slot></slot></div>
--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -1,4 +1,4 @@
 <script>
-  import "../app.css";
+  import "$lib/app.css";
 </script>
-<div class={"overflow-auto bg-gradient-to-br from-cyan-100 to-blue-50 h-screen w-screen"}><slot></slot></div>
+<slot></slot>
