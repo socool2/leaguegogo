8e080e683af8668324f69a6f6d95d67a708a5f1f
Yxnzoo <anuefn@naver.com>
2023-08-19 14:37:19 +0900

    Add League info update view

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -4,11 +4,17 @@
 
 <div class="flex flex-col text-left items-center text-2xl p-8 gap-4 text-gray-600 underline">
   <a href="/admin">관리자 로그인-설치 후 최초 로그인</a>
-  <a href="/admin/league/register">
+  <a href="/league/register">
     로그인 > 관리자 로그인 > 첫 리그 정보 등록
+  </a>
+  <a href="/league/update">
+    로그인 > 관리자 로그인 > 리그 관리 > 리그 정보 수정
   </a>
   <a href="/season/register">
     로그인 > 관리자 로그인 > 리그 정보 등록 > 첫 시즌 정보 등록
+  </a>
+  <a href="/season/list">
+    로그인 > 관리자 로그인 > 시즌 목록
   </a>
   <a href="/main">
     초기화면
