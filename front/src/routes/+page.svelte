da3783d6e40a30a66152899f2f50eeee5fdfa89e
JC Choi <godofslp@gmail.com>
2023-08-19 14:14:58 +0900

    첫 리그 정보 등록 경로 수정
    /admin/league/register -> /league/register

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <div class="flex flex-col text-left items-center text-2xl p-8 gap-4 text-gray-600 underline">
   <a href="/admin">관리자 로그인-설치 후 최초 로그인</a>
-  <a href="/admin/league/register">
+  <a href="/league/register">
     로그인 > 관리자 로그인 > 첫 리그 정보 등록
   </a>
   <a href="/season/register">
